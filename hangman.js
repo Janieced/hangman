f7f6aa14ddba4b3f93e2707e6216d4969508892b
Janiece Dickens <janiecedickens@Janieces-MacBook-Pro.local>
2018-08-14 22:26:44 -0400

    hangman.js

---
+++ hangman.js
@@ -0,0 +1,35 @@
+//Create an array of sneaker names to choose from
+var sneakers = ["Foamposite", "Presto", "Huarache","Yeezy","Promodel","AirMax", "Vapormax","Vans","UltraBoost","Iversons"]
+
+
+//Create variables that will be used throughout the code
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var dashes = [""]
+
+//Create a function that allows the game to start when a key is pressed
+$(document).ready (function() 
+
+
+//Create a function that randomly chooses a sneaker from the array
+$(randomIndex) = Math.floor(Math.random() * sneakers.length)
+$(randomElement = sneakers["string"]
+
+
+//Create a funtion that takes the number of letters in the word and makes dashes appear
+
+
+//Create a function that makes the letter appear over the dash when it is selected by the user
+
+
+//Create a funtion that makes the incorrect letter appear in the "Letters Chosen" line
+
+
+//Create a function that makes the "Guesses Remaining" number update after every incorrect guess
+
+
+//Create a function that updates the "Wins" line by 1 if the user wins
+
+
+//Create a function that updates the "Losses" line by 1 if the user loses
